381af3586f14828379b8b09d68d650c05f5cc988
Manten Devillé <manten.d@hotmail.com>
2021-02-26 15:57:32 +0100

    Change create function

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 declare(strict_types = 1);
 
 // Show errors so we get helpful information
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 
 // Load you classes
 require_once 'config.php';
